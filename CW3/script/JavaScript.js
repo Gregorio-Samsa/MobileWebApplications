0ed75464b743909460d0bc570a09151125ab78f9
Marlon Gaitan Laptop <marlongb2002@gmail.com>
2023-02-15 21:28:05 -0600

    Class Work 3

---
+++ CW3/script/JavaScript.js
@@ -0,0 +1,85 @@
+vid.onclick = function(e){
+    document.getElementById("computer").style.display='none';
+    document.getElementById("videogames").style.display='flex';
+    document.getElementById("books").style.display='none';
+    document.getElementById("Textbooks").style.display='none';
+    document.getElementById("Men").style.display='none';
+    document.getElementById("Women").style.display='none';
+}
+comp.onclick = function(e){
+    document.getElementById("computer").style.display='flex';
+    document.getElementById("videogames").style.display='none';
+    document.getElementById("books").style.display='none';
+    document.getElementById("Textbooks").style.display='none';
+    document.getElementById("Men").style.display='none';
+    document.getElementById("Women").style.display='none';
+}
+bks.onclick = function(e){
+    document.getElementById("computer").style.display='none';
+    document.getElementById("videogames").style.display='none';
+    document.getElementById("books").style.display='flex';
+    document.getElementById("Textbooks").style.display='none';
+    document.getElementById("Men").style.display='none';
+    document.getElementById("Women").style.display='none';
+}
+Tbks.onclick = function(e){
+    document.getElementById("computer").style.display='none';
+    document.getElementById("videogames").style.display='none';
+    document.getElementById("books").style.display='none';
+    document.getElementById("Textbooks").style.display='flex';
+    document.getElementById("Men").style.display='none';
+    document.getElementById("Women").style.display='none';
+}
+m.onclick = function(e){
+    document.getElementById("computer").style.display='none';
+    document.getElementById("videogames").style.display='none';
+    document.getElementById("books").style.display='none';
+    document.getElementById("Textbooks").style.display='none';
+    document.getElementById("Men").style.display='flex';
+    document.getElementById("Women").style.display='none';
+}
+w.onclick = function(e){
+    document.getElementById("computer").style.display='none';
+    document.getElementById("videogames").style.display='none';
+    document.getElementById("books").style.display='none';
+    document.getElementById("Textbooks").style.display='none';
+    document.getElementById("Men").style.display='none';
+    document.getElementById("Women").style.display='flex';
+}
+
+comp0.onclick = function(e){
+    document.getElementById("popContainer0").style.display="block";
+}
+popContainer0.onclick = function(e){
+    document.getElementById("popContainer0").style.display="none";
+}
+comp1.onclick = function(e){
+    document.getElementById("popContainer1").style.display="block";
+}
+popContainer1.onclick = function(e){
+    document.getElementById("popContainer1").style.display="none";
+}
+comp2.onclick = function(e){
+    document.getElementById("popContainer2").style.display="block";
+}
+popContainer2.onclick = function(e){
+    document.getElementById("popContainer2").style.display="none";
+}
+comp3.onclick = function(e){
+    document.getElementById("popContainer3").style.display="block";
+}
+popContainer3.onclick = function(e){
+    document.getElementById("popContainer3").style.display="none";
+}
+comp4.onclick = function(e){
+    document.getElementById("popContainer4").style.display="block";
+}
+popContainer4.onclick = function(e){
+    document.getElementById("popContainer4").style.display="none";
+}
+comp5.onclick = function(e){
+    document.getElementById("popContainer5").style.display="block";
+}
+popContainer5.onclick = function(e){
+    document.getElementById("popContainer5").style.display="none";
+}
